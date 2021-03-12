6858907a13027d3c3b23aab0d634b688c7c3a261
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2021-03-12 17:47:08 -0300

    feat: Create user account

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
-const express = require('express');
+import express, { json } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
+
+app.use(json());
+
+const customers = [];
+
+app.post('/account', (request, response) => {
+  const { name, cpf } = request.body;
+
+  const id = uuidv4();
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
 
 app.listen(3333);
