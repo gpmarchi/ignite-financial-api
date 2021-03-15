05c3ce07097d01a9e9a40a7ed0f256537611480c
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2021-03-15 16:44:12 -0300

    feat: Delete account and show balance

--- src/index.js
+++ src/index.js
@@ -129,4 +129,20 @@ app.get('/account', verifyExistingAccount, (request, response) => {
   return response.json(customer);
 });
 
+app.delete('/account', verifyExistingAccount, (request, response) => {
+  const { customer } = request;
+
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customers);
+});
+
+app.get('/balance', verifyExistingAccount, (request, response) => {
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  return response.json({ balance });
+});
+
 app.listen(3333);
