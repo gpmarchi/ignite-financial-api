0f6dbedc49f6be9d50043ee98f12479a9751f5f8
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2021-03-15 13:09:32 -0300

    feat: Allow customer to withdraw from account

--- src/index.js
+++ src/index.js
@@ -21,6 +21,18 @@ function verifyExistingAccount(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((accumulator, operation) => {
+    if (operation.type === 'credit') {
+      return accumulator + operation.amount;
+    }
+
+    return accumulator - operation.amount;
+  }, 0);
+
+  return balance;
+}
+
 app.post('/account', (request, response) => {
   const { name, cpf } = request.body;
 
@@ -54,7 +66,30 @@ app.post('/deposit', verifyExistingAccount, (request, response) => {
   const statementOperation = {
     description,
     amount,
-    type: "credit",
+    type: 'credit',
+    created_at: new Date(),
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.post('/withdrawal', verifyExistingAccount, (request, response) => {
+  const { amount, description } = request.body;
+
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: 'Insufficient funds.' });
+  }
+
+  const statementOperation = {
+    description,
+    amount,
+    type: 'debit',
     created_at: new Date(),
   };
 
